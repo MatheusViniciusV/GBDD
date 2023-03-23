3465dff95d55e52b66a580f560639fc3c92bc28b
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2023-03-23 12:54:21 -0300

    Preparando para inserir as barras de rolagem

--- GUITable.py
+++ GUITable.py
@@ -2,24 +2,25 @@ from tkinter import *
 from tkinter import ttk
 import sqlite3
 
-
 class GUITable:
 
     __root = None
-    __frame = None
+    __mainframe = None
     __connection = None
     __cursor = None
     __treeview = None
     __columns = []
+    __xscroll = None
+    __yscroll = None
 
     def __init__(self, databasename, tablename):
         self.__root = Tk()
         self.__root.title(databasename + ' - ' + tablename)
-        self.__root.geometry('600x400')
+        self.__root.geometry('1200x800')
         self.__root.resizable(True, True)
 
-        self.__frame = LabelFrame(self.__root, text=databasename + ' - ' + tablename)
-        self.__frame.pack(fill='both', padx=10, pady=10)
+        self.__frame = Frame(self.__root)
+        self.__frame.pack(fill='both', expand=1)
 
         self.__connection = sqlite3.connect(databasename + '.db')
         self.__cursor = self.__connection.cursor()
@@ -33,9 +34,10 @@ class GUITable:
             self.__columns.append(i[0])
 
         self.__treeview = ttk.Treeview(self.__frame, columns=self.__columns, show='headings')
+        self.__treeview.pack(fill='both')
 
         for i in self.__columns:
-            self.__treeview.column(i, minwidth=50)
+            self.__treeview.column(i, minwidth=50, width=50)
             self.__treeview.heading(i, text=i)
 
         self.__treeview.pack()
@@ -44,6 +46,9 @@ class GUITable:
         rows = self.__cursor.fetchall()
         for i in rows:
             self.__treeview.insert('', 'end', values=i)
+
+    def __initscrolls(self):
+        #ainda para ser feito
 
     def __exit(self): 
         self.__root.destroy() 
