b03687290fe2c2a76733fad741768f3d391f50c6
VPom135 <51012581+VPom135@users.noreply.github.com>
2023-03-27 19:09:43 -0300

    merge gui.py e main.py

--- main.py
+++ main.py
@@ -1,6 +1,122 @@
-from tkinter import *
-from SQLmain import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as dlg
 
+import sqlite3
+import os
+
+connection = None
+cursor = None
+
+
+class Gui:
+    root = tk.Tk()
+    tabcontrol = ttk.Notebook(root)
+    tabs = []
+    inicio = None
+
+    def __init__(self):
+        self.root.title('DDM')
+        self.root.geometry('600x320')
+        self.root.resizable(False, False)
+
+        self.inittabs()
+
+    def inittabs(self):
+        self.inicio = Inicio(gui=self, tabcontrol=self.tabcontrol)
+        self.addtab(self.inicio.frame, 'Início')
+        self.tabcontrol.pack(expand=1, fill="both")
+
+    def addtab(self, tab, name):
+        self.tabs.append(tab)
+        self.tabcontrol.add(self.tabs[len(self.tabs) - 1], text=name)
+
+    def exit(self):
+        self.root.destroy()
+
+    def run(self):
+        self.root.mainloop()
+
+
+class Inicio:
+    frame = None
+    buttonopen = None
+    buttonclose = None
+    buttonnew = None
+    buttondelete = None
+    buttontablecreate = None
+    buttontabledelete = None
+    buttontableopen = None
+    stringvarlabeldb = None
+    labelframe = None
+    labeldb = None
+    lablestable = []
+    listtables = ()
+    listbox = None
+    var = None
+    file = None
+    gui = None
+
+    def __init__(self, gui, tabcontrol):
+        self.frame = ttk.Frame(tabcontrol)
+        self.buttonopen = ttk.Button(self.frame, text='Abrir banco de dados', command=self.opendb).grid(column=0, row=0,
+                                                                                                        padx=10,
+                                                                                                        pady=10)
+        self.buttonclose = ttk.Button(self.frame, text='Fechar banco de dados', command=self.closedb).grid(column=1,
+                                                                                                           row=0,
+                                                                                                           padx=10,
+                                                                                                           pady=10)
+        self.buttonnew = ttk.Button(self.frame, text='Novo banco de dados', command=self.debug).grid(column=2, row=0, padx=10,
+                                                                                               pady=10)
+        self.buttondelete = ttk.Button(self.frame, text='Deletar banco de dados', command=None).grid(column=3, row=0,
+                                                                                                     padx=10, pady=10)
+        self.buttontablecreate = ttk.Button(self.frame, text='Criar tabela', command=None).grid(sticky=tk.S, column=1,
+                                                                                                row=2, padx=10, pady=10)
+        self.buttontabledelete = ttk.Button(self.frame, text='Deletar tabela', command=None).grid(column=1, row=3,
+                                                                                                  padx=10, pady=10)
+        self.buttontableopen = ttk.Button(self.frame, text='Abrir tabela', command=None).grid(sticky=tk.N, column=1,
+                                                                                              row=4, padx=10, pady=10)
+        self.stringvarlabeldb = tk.StringVar()
+        self.stringvarlabeldb.set('Ainda não há nenhum banco de dados aberto.')
+        self.labeldb = ttk.Label(self.frame, textvariable=self.stringvarlabeldb).grid(sticky=tk.W, column=0, row=1,
+                                                                                      padx=10, pady=10, columnspan=2)
+        self.var = tk.StringVar()
+        self.var.set(self.listtables)
+        self.listbox = tk.Listbox(self.frame, listvariable=self.var).grid(sticky=tk.W, column=0, row=2, padx=10,
+                                                                          pady=15, rowspan=3)
+        self.labelframe = tk.LabelFrame(self.frame, text='Informações da tabela selecionada', width=280,
+                                        height=220).grid(column=2, row=1, padx=10, pady=10, columnspan=2, rowspan=4)
+        self.lablestable.append(ttk.Label(self.labelframe, text='Nome: ').place(x=312, y=100))
+        self.lablestable.append(ttk.Label(self.labelframe, text='Número de colunas: ').place(x=312, y=120))
+        self.lablestable.append(ttk.Label(self.labelframe, text='Número de linhas: ').place(x=312, y=140))
+        self.lablestable.append(
+            ttk.Label(self.labelframe, text='Nome das colunas: ', wraplength=265).place(x=312, y=160))
+        self.gui = gui
+
+    def configlistbox(self):
+        for i in self.listtables:
+            self.listbox.insert('end', i)
+
+    def opendb(self):
+        self.file = dlg.askopenfilename()
+        self.stringvarlabeldb.set('Banco de dados: ' + os.path.split(self.file)[1])
+        global connection
+        global cursor
+        connection = sqlite3.connect(self.file)
+        cursor = connection.cursor()
+        print("openado")
+
+    def closedb(self):
+        self.stringvarlabeldb.set('Ainda não há nenhum banco de dados aberto.')
+
+    def debug(self):
+        cursor.execute('''SELECT name FROM sqlite_master WHERE type='table';''')
+        self.listtables = cursor.fetchall()
+        print(self.listtables)
+
+
+
+'''
 def cadastro():
     newrow(entry_nome.get(),
            entry_nomecurto.get(),
@@ -63,4 +179,8 @@ class GUIMain:
     botao_cadastro.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 
 main = GUIMain()
+main.run()
+'''
+
+main = Gui()
 main.run()
